a1e362e310dd1982611102adc72fd91d0a74098d
GuyR <guyr@wix.com>
2015-02-27 19:48:00 +0200

    graph style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 requirejs.config({
 //    baseUrl: '/',
     paths: {
-        lodash: 'http://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.0.1/lodash.min',
+        lodash: 'http://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.3.1/lodash.min',
         react: 'http://fb.me/react-with-addons-0.12.2',
         zepto: 'http://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.4/zepto.min'
     },
