5671d46b12af6ddbf56703e5f08a169a69857aaf
GuyR <guyr@wix.com>
2015-02-27 18:03:30 +0200

    add styles file

--- src/hello.js
+++ src/hello.js
@@ -1,8 +1,18 @@
-define(['react', 'lodash', './hello.rt'], function (React, _, template) {
+define(['react', 'lodash', 'zepto', './hello.rt'], function (React, _, $, template) {
     'use strict';
 
     return React.createClass({
         displayName: 'Hello',
+        getInitialState: function () {
+            return {data: null};
+        },
+        componentDidMount: function(){
+            $.get('../exampleData.json', function(dependenciesData){
+                this.setState({
+                    data: JSON.parse(dependenciesData)
+                });
+            }.bind(this));
+        },
         render: template
     });
 });
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 requirejs.config({
 //    baseUrl: '/',
     paths: {
-        lodash: '//cdnjs.cloudflare.com/ajax/libs/lodash.js/3.0.1/lodash.min',
-        react: '//fb.me/react-with-addons-0.12.2',
-        zepto: '//cdnjs.cloudflare.com/ajax/libs/zepto/1.1.4/zepto.min.js'
+        lodash: 'http://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.0.1/lodash.min',
+        react: 'http://fb.me/react-with-addons-0.12.2',
+        zepto: 'http://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.4/zepto.min'
     },
     shim: {
         lodash: {exports: '_'},
