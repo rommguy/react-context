874a04053ce3a79b2e757212a1a38fb029fb63b1
GuyR <guyr@wix.com>
2015-02-27 14:33:55 +0200

    add zepto

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 requirejs.config({
 //    baseUrl: '/',
     paths: {
-        lodash: '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min',
-        react: '//fb.me/react-with-addons-0.12.2'
+        lodash: '//cdnjs.cloudflare.com/ajax/libs/lodash.js/3.0.1/lodash.min',
+        react: '//fb.me/react-with-addons-0.12.2',
+        zepto: '//cdnjs.cloudflare.com/ajax/libs/zepto/1.1.4/zepto.min.js'
     },
     shim: {
         lodash: {exports: '_'},
-        react: {exports: 'React'}
+        react: {exports: 'React'},
+        zepto: {exports: '$'}
     },
     map: {
         '*': {
